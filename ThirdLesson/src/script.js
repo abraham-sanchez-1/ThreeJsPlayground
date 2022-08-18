154234edfb5b3168296c6054569c33e80cc30929
Abe Sanchez <sanchez95san@gmail.com>
2022-08-17 22:17:14 -0500

    multiple objects added as group

--- ThirdLesson/src/script.js
+++ ThirdLesson/src/script.js
@@ -2,29 +2,54 @@ import './style.css'
 import * as THREE from 'three'
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
+const group = new THREE.Group();
+group.position.y = 3;
+group.scale.x = 1;
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xff0000})
+)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0022ff})
+)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x0022ff})
+)
+cube2.position.x = -2;
+cube3.position.x = 2;
+
+group.add(cube1)
+group.add(cube2)
+group.add(cube3)
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
-// Position
-mesh.position.set(0.7, -0.6, .5)
+// // Position
+// mesh.position.set(0.7, -0.6, .5)
 
-// Scale
-mesh.scale.set(2, 0.5, 0.5)
+// // Scale
+// mesh.scale.set(2, 0.5, 0.5)
 
-// Rotation
-mesh.rotation.reorder('YXZ');
-mesh.rotation.x = Math.PI *.25;
-mesh.rotation.y = Math.PI *.25;
+// // Rotation
+// mesh.rotation.reorder('YXZ');
+// mesh.rotation.x = Math.PI *.25;
+// mesh.rotation.y = Math.PI *.25;
 
 
 // Axes helper
@@ -47,7 +72,7 @@ camera.position.set(1, 1, 4);
 
 scene.add(camera);
 
-camera.lookAt(mesh.position)
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
